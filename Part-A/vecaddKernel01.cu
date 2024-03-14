a7953a5c464149ac7952c5834f0ec7a861ff5080
yond5413 <yd2696@columbia.edu>
2024-03-14 17:03:30 -0400

    Update vecaddKernel01.cu

--- Part-A/vecaddKernel01.cu
+++ Part-A/vecaddKernel01.cu
@@ -14,7 +14,7 @@ __global__ void AddVectors(const float* A, const float* B, float* C, int N)
     int blockStartIndex = blockIdx.x * blockDim.x * N;
     int threadStartIndex = blockStartIndex + (threadIdx.x * N);
     int threadEndIndex = threadStartIndex + N;
-
+    int i;
     for (int i = threadStartIndex; i < threadEndIndex; ++i) {
         if (i < N) {
             C[i] = A[i] + B[i];
