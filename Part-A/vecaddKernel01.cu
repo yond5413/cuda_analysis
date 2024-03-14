b6b4eb5c80051e40535173384bee3635f4f40b8a
yond5413 <yd2696@columbia.edu>
2024-03-14 16:19:56 -0400

    Update vecaddKernel01.cu

--- Part-A/vecaddKernel01.cu
+++ Part-A/vecaddKernel01.cu
@@ -14,6 +14,6 @@ __global__ void AddVectors(const float* A, const float* B, float* C, int N)
     //int index = (blockDim.x * blockIdx.x) + threadIdx.x;
     int i = threadIdx.x;
     if(i<N)
-    C[i] = A[i]+B[i]
+    C[i] = A[i]+B[i];
     
 }
