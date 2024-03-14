f61914693b3f394f2ed352062d705629d47da30e
yond5413 <yd2696@columbia.edu>
2024-03-14 05:28:10 -0400

    Create vecaddKernel01.cu

---
+++ Part-A/vecaddKernel01.cu
@@ -0,0 +1,17 @@
+/*
+/// vecAddKernel00.cu
+/// For COMS E6998 Spring 2024
+/// Instructor: Kaoutar El Maghraoui
+/// By Yonathan Daniel 
+Created: 2024-03-14
+/// This Kernel adds two Vectors A and B in C on GPU
+/// with coalesced memory access.
+/// 
+*/
+
+__global__ void AddVectors(const float* A, const float* B, float* C, int N)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < N)
+        C[i] = A[i] + B[i];
+}
