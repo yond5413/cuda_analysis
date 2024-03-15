7fac96f40513acad7a796290270e4857af4fec36
yond5413 <yd2696@columbia.edu>
2024-03-14 23:00:52 -0400

    Update vecaddKernel01.cu

--- Part-A/vecaddKernel01.cu
+++ Part-A/vecaddKernel01.cu
@@ -16,13 +16,14 @@ __global__ void AddVectors(const float* A, const float* B, float* C, int N)
     gridDim provides number of blocks
     blocks provides number of threads
     */
-    int foo = blockDim.x*gridDim.x*N; // Number of vector elements?
-    int i = threadIdx.x + (blockIdx.x*blockDim.x);
+    //int foo = blockDim.x*gridDim.x*N; // Number of vector elements?
+    int i = threadIdx.x*N + (blockIdx.x*blockDim.x);
     printf("blockDim: %d, gridDim: %d, i:%d, N:%d, foo: %d, tid: %d, blockid: %d \n",blockDim.x,gridDim.x,i, N, foo,threadIdx.x,blockIdx.x);
     //printf("tid: %d, blockid: %d \n", threadIdx.x,blockIdx.x);
-    while(i<N){
+    //while(i<N){
+    if(i<N)
         C[i] = A[i]+B[i];
-        i+= blockDim.x*gridDim.x;
-    }
+        //i+= blockDim.x*gridDim.x;
+//}
 }
     
