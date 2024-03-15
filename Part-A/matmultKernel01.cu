7d638d61f40d26c254ad94a6a2bb4d4535ffc8f3
yond5413 <yd2696@columbia.edu>
2024-03-15 00:19:31 -0400

    updated Makefile

---
+++ Part-A/matmultKernel01.cu
@@ -0,0 +1,12 @@
+/*
+For COMS E6998(HPML) Spring 2024
+*/
+#include "matmultKernel.h"
+
+#define FOOTPRINT_SIZE BLOCK_SIZE
+
+// Define a gpu kernel to perform matrix multiplication
+// of A x B = C.
+__global__ void MatMulKernel(Matrix A, Matrix B, Matrix C){
+
+}
