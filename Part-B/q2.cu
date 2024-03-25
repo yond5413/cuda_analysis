ad67ee63aa2c081cfbf51e0e9389c08bc96a1d2d
yond5413 <yd2696@columbia.edu>
2024-03-25 15:32:32 -0400

    Update q2.cu

--- Part-B/q2.cu
+++ Part-B/q2.cu
@@ -11,12 +11,9 @@ ___global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int i = threadIdx.x + (blockIdx.x*blockDim.x);
     int stride = blockDim.x*gridDim.x;
-    //printf("blockDim: %d, gridDim: %d, i:%d, N:%d, foo: %d, tid: %d, blockid: %d \n",blockDim.x,gridDim.x,i, N, foo,threadIdx.x,blockIdx.x);
-    //printf("tid: %d, blockid: %d \n", threadIdx.x,blockIdx.x);
     while(i<N*stride){
         if(i<N*stride){
         C[i] = A[i]+B[i];
-        //printf("i: %d, i+stride: %d, N: %d, foo:%d %d\n",i,(i+stride),N,blockDim.y,gridDim.y);
         }
         i+=stride;
     }
