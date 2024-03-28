346fa920d237f5c8188e0b5bb48ca07108218feb
yond5413 <yd2696@columbia.edu>
2024-03-28 05:39:07 -0400

    Update c1.cu

--- Part-C/c1.cu
+++ Part-C/c1.cu
@@ -66,10 +66,10 @@ int main(int argc, char* argv[]){
     size_t size_F = FH*FW*C*K;
     size_t size_O = K*H*W;
     printf("Malloc \n");
-    h_I = (double*)malloc(size_I);
-    h_F = (double*)malloc(size_F);
-    h_O = (double*)malloc(size_O);
-    h_Io = (double*)malloc(size_Io);
+    h_I = (double*)malloc(size_I*sizeof(double));
+    h_F = (double*)malloc(size_F*sizeof(double));
+    h_O = (double*)malloc(size_O*sizeof(double));
+    h_Io = (double*)malloc(size_Io*sizeof(double));
     printf("init?\n");
     // init I tensor
     // Initialize I
